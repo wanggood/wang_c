e15f9a26d2ef7b27f9355745076b118b36114a52
wanggood <453604341@qq.com>
2015-06-03 20:31:46 +0800

    nginx config

---
+++ nginx/conf/domains/test2_redis.lua
@@ -0,0 +1,37 @@
+local redis = require "resty.redis" 
+local cache = redis.new()
+local ok, err = cache.connect(cache, '127.0.0.1', '6379')
+cache:set_timeout(60000)  
+
+if not ok then  
+    ngx.say("failed to connect:", err)  
+    return  
+end  
+res, err = cache:set("dog", "an aniaml")  
+
+if not ok then
+    ngx.say("failed to set dog: ", err)
+    return 
+end
+
+ngx.say("set result: ",res)
+
+local res, err = cache:get("dog")
+if not res then 
+     ngx.say("failed to get dog: ", err)
+     return 
+end
+
+if res == ngx.null then
+     ngx.say("dog not found.")
+     return 
+end
+
+ngx.say("dog: ",res)
+
+local ok, err =cache:close()
+
+if not ok then
+     ngx.say("failed to close:", err)
+     return 
+end
